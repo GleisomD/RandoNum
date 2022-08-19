7c744406ebbd34024c5daf263936d0cdc6b44850
Gleisom <gleisom.silva@dp6.com.br>
2022-08-19 14:20:51 -0300

    Added body prop

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,12 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>RandoNum</h1>
-      <p>Seu gerador de números aleatórios</p>
-      <Form/>
+    <div className={styles.body}>
+      <div className={styles.container}>
+        <h1>RandoNum</h1>
+        <p>Seu gerador de números aleatórios</p>
+        <Form/>
+      </div>
     </div>
   )
 }
