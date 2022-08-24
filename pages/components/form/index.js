ef386a3b159edbf8aa4606576d7f120697c8061d
Gleisom <gleisom.silva@dp6.com.br>
2022-08-24 14:16:47 -0300

    Edited file

--- pages/components/form/index.js
+++ pages/components/form/index.js
@@ -3,10 +3,10 @@ import React, { Fragment } from "react"
 
 const Form = () => {
     return (
-        <form id="form">
-                <label for="minNum">Escolha um número:</label>
+        <form>
+                <label htmlFor="minNum">Escolha um número:</label>
                 <input type="text" id="minNum"></input>
-                <label for="maxNum">Digite mais um número:</label>
+                <label htmlFor="maxNum">Digite mais um número:</label>
                 <input type="text" id="maxNum"></input>
                 <button type="submit">Gerar!</button>    
         </form>
